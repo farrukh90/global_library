41cbcf12bfb1dc5ad8681b40fa6a9933c845e01b
farrukh.sadykov <farrukh.sadykov@capgemini.com>
2021-04-25 17:20:11 -0500

    added artemis

--- src/com/lib/artemis.groovy
+++ src/com/lib/artemis.groovy
@@ -1,44 +1,43 @@
-def deployer(ENVIR){
+def deployer(ENVIR, REPO){
 	properties([parameters([choice(choices: ['dev1.acirrustech.com', 'qa1.acirrustech.com', 'stage1.acirrustech.com', 'prod1.acirrustech.com'], description: 'Please enter an environment', name: 'ENVIR')])])
     node {
-	  
-	stage("Copy Artemis"){
-		timestamps {
-		ws {
-			git  'https://github.com/fuchicorp/artemis.git'
+		stage("Copy Artemis"){
+			timestamps {
+				ws {
+					git  "${REPO}"
+				}
+			}
 		}
-	}
-	}
-	stage("Install Prerequisites"){
-		timestamps {
-			ws{
-				sh "ssh centos@${ENVIR} sudo yum install epel-release -y"
-				sh "ssh centos@${ENVIR} sudo yum install python-pip -y"
-				sh "ssh centos@${ENVIR} sudo pip install Flask"
+		stage("Install Prerequisites"){
+			timestamps {
+				ws{
+					sh "ssh centos@${ENVIR} sudo yum install epel-release -y"
+					sh "ssh centos@${ENVIR} sudo yum install python-pip -y"
+					sh "ssh centos@${ENVIR} sudo pip install Flask"
+				}
+			}
 		}
-	}
-}
-	stage("Copy Artemis"){
-		timestamps {
-		ws {
-			sh "scp -r * centos@${ENVIR}:/tmp"
+		stage("Copy Artemis"){
+			timestamps {
+				ws {
+					sh "scp -r * centos@${ENVIR}:/tmp"
+				}
+			}
 		}
-	}
-}
-	stage("Run Artemis"){
-		timestamps {
-			ws {
-				sh "ssh centos@${ENVIR} nohup python /tmp/artemis.py & "
+		stage("Run Artemis"){
+			timestamps {
+					ws {
+						sh "ssh centos@${ENVIR} nohup python /tmp/artemis.py & "
+				}
+			}
 		}
-	}
-}
-	stage("Send slack notifications"){
-		timestamps {
-			ws {
-				echo "Slack"
-				//slackSend color: '#BADA55', message: 'Hello, World!'
+		stage("Send slack notifications"){
+			timestamps {
+				ws {
+					echo "Slack"
+					//slackSend color: '#BADA55', message: 'Hello, World!'
+				}
 			}
 		}
 	}
-}
 }
