cf1d328610931049830fa880d95061371a7b5277
farrukh.sadykov <farrukh.sadykov@capgemini.com>
2021-04-25 19:22:40 -0500

    removd "

--- src/com/lib/artemis.groovy
+++ src/com/lib/artemis.groovy
@@ -3,7 +3,7 @@ def deployer(ENVIR, REPO, APP_VERSION){
 		parameters([
 			choice(choices: ['18.206.206.46', 'qa1.acirrustech.com', 'stage1.acirrustech.com', 'prod1.acirrustech.com'], description: 'Please enter an environment', name: 'ENVIR'),
 			string(defaultValue: 'https://github.com/farrukh90/artemis.git', description: 'https://github.com/farrukh90/artemis.git', name: 'REPO', trim: false),
-			string(defaultValue: '0.1', description: 'Please specify App Version', name: "APP_VERSION", trim: false)
+			string(defaultValue: '0.1', description: 'Please specify App Version', name: 'APP_VERSION', trim: false)
 			])
 		])
 	node {
