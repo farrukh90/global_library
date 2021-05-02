0b7668273886d0c9b90b895eeb30c53950d06d82
Farrukh Sadykov <farrukhsadykov@gmail.com>
2021-05-01 23:11:13 -0500

    Create docker.groovy

---
+++ src/com/lib/docker.groovy
@@ -0,0 +1,22 @@
+def deploy(){
+    node{
+      currentBuild.result = 'SUCCESS'
+      stage("Git Pull"){
+          checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/Flaskex.git']]])
+      }
+      stage("Build"){
+          sh "docker build -t farrukhsadykov/flaskex:${env.BUILD_NUMBER} ."
+      }
+      stage("Docker Login"){
+          withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+              sh """
+                  docker login -u ${USERNAME} -p ${PASSWORD} 
+                  docker push farrukhsadykov/flaskex:"${env.BUILD_NUMBER}"
+              """
+          }   
+      }
+      stage("test"){
+          echo "hello"
+      }
+  }
+}
