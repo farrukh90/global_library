13029d6cd7ce6cf9e82d1d6c465ec98b997ca40d
farrukh90 <farrukhsadykov@gmail.com>
2021-09-16 17:18:46 +0000

    added src

---
+++ src/com/lib/for_everyone.groovy
@@ -0,0 +1,19 @@
+def imageBuilder(REPO)
+    node {
+        stage("Clone a Repo"){
+            checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: ${REPO} ]]])
+        }
+        stage("Validate"){
+            ws("${workspace}/packer/pipelines/tools"){
+                sh "packer validate golden_image.json"
+            }
+        }
+        stage("Build Image"){
+            ws("${workspace}/packer/pipelines/tools"){
+                sh "packer build golden_image.json"
+            }
+        }
+        stage("Send Notification"){
+            echo "Hello"
+        }
+    }
